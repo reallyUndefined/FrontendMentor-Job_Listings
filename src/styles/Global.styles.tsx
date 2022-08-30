dacb4b87e2bc7fe859b2960389d89467c931291e
Prabu <prabu.undefined@gmail.com>
2022-08-30 11:56:40 +0530

    project setup

---
+++ src/styles/Global.styles.tsx
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import { lightGrayishCyanBg } from "./colors.styles";
+
+export const GlobalStyles = createGlobalStyle`
+  *, *::before, *::after {
+    padding: 0;
+    margin: 0;
+    box-sizing:border-box;
+  }
+
+  body {
+    min-height: 100vh;
+    font-family: 'League Spartan', sans-serif;
+    background-color: ${lightGrayishCyanBg};
+  }
+`;
---
+++ src/styles/colors.styles.ts
@@ -0,0 +1,8 @@
+// primary
+export const desaturatedDarkCyan = "hsl(180, 29%, 50%)";
+
+// neutral
+export const lightGrayishCyanBg = "hsl(180, 52%, 96%)";
+export const lightGrayishCyanTabs = "hsl(180, 31%, 95%)";
+export const darkGrayishCyan = "hsl(180, 8%, 52%)";
+export const veryDarkGrayishCyan = "hsl(180, 14%, 20%)";
