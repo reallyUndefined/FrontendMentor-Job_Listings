2243997e7ffed60a652c14f93d75fc69e26dc808
Prabu <prabu.undefined@gmail.com>
2022-08-31 13:58:28 +0530

    pill color fix

--- src/components/pill/Pill.styles.tsx
+++ src/components/pill/Pill.styles.tsx
@@ -5,7 +5,7 @@ interface SPillProps {
 }
 export const SPill = styled.div<SPillProps>`
   border-radius: 99rem;
-  background-color: ${({ color }) => color};
+  background-color: ${({ bgColor }) => bgColor};
   color: white;
   padding-inline: 0.5rem;
   padding-block: 0.25rem;
